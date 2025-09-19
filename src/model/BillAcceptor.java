1133cc99ba51e408644eb7308e943256de2271e2
Askarbekova <.@>
2025-09-19 15:57:35 +0600

    Add a method "addAmount" and implemented the interface

--- src/model/BillAcceptor.java
+++ src/model/BillAcceptor.java
@@ -1,4 +1,42 @@
 package model;
 
-public class BillAcceptor {
+import java.util.Scanner;
+
+public class BillAcceptor implements PaymentAcceptor {
+    private int amount = 0;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void addAmount() {
+        System.out.print("Введите купюру (50, 100, 200): ");
+        int bill = scanner.nextInt();
+        if (bill == 50 || bill == 100 || bill == 200) {
+            amount += bill;
+            System.out.println("Баланс пополнен на " + bill + ". Текущий баланс: " + amount);
+        } else {
+            System.out.println("Автомат не принимает такую купюру!");
+        }
+    }
+
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void addAmount(int amount) {
+
+    }
+
+    @Override
+    public boolean canAfford(int price) {
+        return amount >= price;
+    }
+
+    @Override
+    public void deductAmount(int price) {
+        if (canAfford(price)) {
+            amount -= price;
+        }
+    }
 }
+
